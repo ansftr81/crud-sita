cce944fefb27124a80d16559e71d022a5e1d2ee1
Arif Rahman <120116183+ariprahman7@users.noreply.github.com>
2023-07-18 00:49:06 +0700

    Menambahakan tugas akhir
    Semangaat sayang smoga suksess

--- TA.php
+++ TA.php
@@ -1,50 +1,42 @@
 <?php
 // Create database connection using config file
 include_once("config.php");
- 
+
 // Fetch all users data from database
-
 $strquery = "SELECT 
-ta.id, ta.judul, m.nama as mahasiswa, d.nama as pembimbing
-
-FROM
-ta, dosen d, mahasiswa m
-
-WHERE
-ta.mahasiswa = m.id
-AND
-ta.pembimbing = d.id";
-
+    ta.id, ta.judul, m.nama as mahasiswa, d.nama as pembimbing
+    FROM
+    ta
+    INNER JOIN dosen d ON ta.pembimbing = d.id
+    INNER JOIN mahasiswa m ON ta.mahasiswa = m.id";
 $result = mysqli_query($mysqli, $strquery);
-
 ?>
- 
+
 <html>
 <head>    
     <title>Data TA</title>
 </head>
  
 <body>
-<a href="index.php">Home</a>|<a href="ta_add.php">Tambah TA</a><br/><br/>
+    <a href="index.php">Home</a> | <a href="ta_add.php">Tambah TA</a><br/><br/>
  
     <table width='80%' border=1>
- 
-    <tr>
-        <th>ID</th>  <th>Judul</th> <th>Mahasiswa</th> <th>Pembimbing</th> <th>Update</th>
-    </tr>
-    <?php  
-    while($user_data = mysqli_fetch_array($result)) {         
-        echo "<tr>";
-        echo "<td>".$user_data['id']."</td>";
-        echo "<td>".$user_data['judul']."</td>";
-        echo "<td>".$user_data['mahasiswa']."</td>";
-        echo "<td>".$user_data['pembimbing']."</td>";    
-        echo "<td>
-                    <a href='TA_edit.php?id=$user_data[id]'>Edit</a> | 
-                    <a href='TA_delete.php?id=$user_data[id]'>Delete</a>
-            </td></tr>";        
-    }
-    ?>
+        <tr>
+            <th>ID</th>  
+            <th>Judul</th> 
+            <th>Mahasiswa</th> 
+            <th>Pembimbing</th> 
+            <th>Update</th>
+        </tr>
+        <?php  
+        while ($row = mysqli_fetch_array($result)) {
+            echo $row['id'];
+            echo $row['judul'];
+            echo $row['mahasiswa'];
+            echo $row['pembimbing'];
+        
+        }
+        ?>
     </table>
 </body>
 </html>
